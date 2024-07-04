0390115b196c48f22f46ea1a60b5a870eca2b8fc
minnnhoi2512 <133669352+minnnhoi2512@users.noreply.github.com>
2024-07-04 10:38:52 +0700

    yesterday-code

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -9,7 +11,32 @@ export default function Home() {
     overflow-hidden
     overflow-y-auto
     ">
-
+      <Header>
+        <div className="
+    mb-2
+    ">
+          <h1 className="
+          text-white
+          text-3xl
+          font-semibold
+          "
+          >Welcome back</h1>
+          <div className="
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          xl:grid-cols-3
+          2xl:grid-cols-4
+          gap-3
+          mt-4
+          ">
+            <ListItem
+              image="/images/liked.jpg"
+              name="Liked Songs"
+              href="liked" />
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
